453183e508a2f187b0f67be046592ece4dbf9b46
Local PC <lowlevelengineer@gmail.com>
2022-05-21 23:57:49 +0900

    commit

--- examples/step3.rs
+++ examples/step3.rs
@@ -1,5 +1,9 @@
 use std::collections::LinkedList;
+use std::fmt::*;
 
+use crate::TokenKind::*;
+
+#[derive(PartialEq)]
 enum TokenKind {
     TK_RESERVED,
     TK_NUM,
@@ -8,19 +12,30 @@ enum TokenKind {
 
 struct Token {
     kind: TokenKind,
-    next: Link,
     val: usize,
-    str: u8,
+    str: String,
 }
 
-type Link = Option<Box<Token>>;
-
-fn error()
+fn error(args: Arguments)
 {
-    ;
+    eprintln!("{}/n", args);
 }
 
-fn consume() -> bool
+fn consume(token: &mut LinkedList<Token>, op: u8) -> bool
 {
-    ;
+    if token.len() > 1
+    {
+        let str = token.pop_front().str.as_bytes();
+        if token.kind != TK_RESERVED || str[0] != op
+        {
+            return false;
+        }
+    }
+    return true;
 }
+
+pub fn main()
+{
+
+}
+
