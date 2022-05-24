6ffa95af7c06d087af752a09934211c824557e08
Local PC <lowlevelengineer@gmail.com>
2022-05-24 23:52:32 +0900

    commit

--- examples/step3.rs
+++ examples/step3.rs
@@ -34,8 +34,61 @@ fn consume(token: &mut LinkedList<Token>, op: u8) -> bool
     return true;
 }
 
-pub fn main()
+fn main()
 {
+    let mut args: Vec<String> = env::args().collect();
 
+    let num = args.len();
+    if num != 2
+    {
+        eprintln!("引数に異常あり：{}", num);
+    }
+
+    let AsmFilename = args.pop().unwrap();
+    let AsmFileReslt = fs::File::open(AsmFilename.clone());
+    match AsmFileReslt
+    {
+        Ok(v) =>{
+            fs::remove_file(AsmFilename.clone()).unwrap();
+        }
+        Err(e) =>{
+        }
+    }
+
+    let mut AsmFile = fs::File::create(AsmFilename.clone()).unwrap();
+    let mut AsmFile = OpenOptions::new().read(true).write(true).open(AsmFilename).unwrap();
+
+    AsmFile.write_all(b".intel_syntax noprefix\n").unwrap();
+    AsmFile.write_all(b".global main\n").unwrap();
+    AsmFile.write_all(b"main:\n").unwrap();
+    AsmFile.write_all(b" mov rax, ").unwrap();
+
+    let Token = tokenize(args.pop().unwrap());
+
+    let mut n = expect_number();
+    AsmFile.write_all(n).unwrap();
+
+    loop
+    {
+        if at_eof() == true
+        {
+            break;
+        }
+
+        if consume('+') == true
+        {
+            AsmFile.write_all(b" add rax, ").unwrap();
+            let mut n = expect_number();
+            AsmFile.write_all(n).unwrap();
+            continue;        
+        }
+
+        expect('-');
+        AsmFile.write_all(b" sub rax, ").unwrap();
+        let mut n = expect_number();
+        AsmFile.write_all(n).unwrap();
+    }
+
+    AsmFile.write_all(b" ret\n").unwrap();
 }
 
